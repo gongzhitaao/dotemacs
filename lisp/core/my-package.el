fde858c3eb0b24a3a2b84683c5a50ef80c98aadd
Zhitao Gong <zhitaao.gong@gmail.com>
2014-05-29 09:51:05 -0500

    org

--- lisp/core/my-package.el
+++ lisp/core/my-package.el
@@ -29,7 +29,8 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
 (defvar my-packages
