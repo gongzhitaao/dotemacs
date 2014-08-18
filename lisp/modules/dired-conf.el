4ce285649ab430cfa739c0a6ca4010c6468c9c37
Zhitao Gong <zhitaao.gong@gmail.com>
2014-08-18 10:40:37 -0500

    gnus other frame

---
+++ lisp/modules/dired-conf.el
@@ -0,0 +1,7 @@
+;;; dired-conf.el --- Dired config
+;;; Time-stamp: <2014-08-01 16:14:19 CDT gongzhitaao>
+
+(setq dired-listing-switches "-alsh")
+
+(provide 'dired-conf)
+;;; dired-conf.el ends here
