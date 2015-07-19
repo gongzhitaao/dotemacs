8f94222881de158f452a098c5534a0682974aab7
Zhitao Gong <zhitaao.gong@gmail.com>
2015-07-19 08:04:38 -0500

    literal configuration in orgmode

--- conf/diary-conf.el
+++
@@ -1,6 +0,0 @@
-;;; diary-conf.el --- Diary config
-;;; Time-stamp: <2015-05-05 20:21:25 CDT gongzhitaao>
-
-(setq diary-file (expand-file-name "diary" my-personal-dir))
-
-;;; diary-conf.el ends here
