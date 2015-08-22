7392c5e0a9b60db868a31139816fdefe144dae57
Zhitao Gong <zhitaao.gong@gmail.com>
2015-08-22 08:26:35 -0500

    remove builtin org

--- init.el
+++ init.el
@@ -3,4 +3,9 @@
 ;;; Time-stamp: <2015-08-05 10:49:38 gongzhitaao>
 ;;; Code:
 
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+(require 'pallet)
+(pallet-mode t)
+(require 'org)
 (org-babel-load-file "~/.emacs.d/README.org")
