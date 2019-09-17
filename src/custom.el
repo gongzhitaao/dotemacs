dbcd6a964f8c4fcf2ad55a8efce3df12ad4c0643
Zhitao Gong <zhitaao.gong@gmail.com>
2019-09-17 17:23:47 -0400

    re-org

---
+++ src/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org tabbar session pod-mode muttrc-mode mutt-alias initsplit graphviz-dot-mode folding eproject csv-mode browse-kill-ring boxquote bm bar-cursor apache-mode yasnippet yapfify yaml-mode writeroom-mode windresize which-key wgrep-helm web-mode volatile-highlights visual-regexp-steroids use-package ssh-config-mode ssh sphinx-doc smartparens simple-call-tree scss-mode rainbow-mode rainbow-delimiters python-docstring prodigy popwin persistent-scratch paren-face pandoc-mode pallet ox-gfm orgalist org-ref org-plus-contrib org-gcal notmuch naquadah-theme multiple-cursors mu4e-alert markup markdown-mode+ magit-popup magit-gh-pulls lua-mode json-mode hl-todo highlight-numbers highlight-indent-guides helm-swoop helm-projectile helm-ls-git helm-gtags helm-flyspell helm-flycheck helm-descbinds helm-dash helm-company helm-ag google-c-style golden-ratio-scroll-screen gnuplot-mode gnuplot gitignore-mode git-timemachine git-gutter git-annex git ggtags fuzzy flymake-python-pyflakes flycheck-tip flycheck-cask fill-column-indicator expand-region exec-path-from-shell evil ess-R-data-view eshell-prompt-extras elisp-slime-nav ein edit-indirect ebib drag-stuff diminish diff-hl delight deft clang-format calfw-org calfw-cal calfw cal-china-x beacon bbdb auctex anzu aggressive-indent ag ace-window ace-jump-mode ace-isearch))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
